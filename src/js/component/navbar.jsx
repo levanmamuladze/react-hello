219ae15d1aa113aac8fa6f879f231caf663363ea
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-04-03 22:07:56 +0200

    update navbar
    changed to bg-dark and added opacity-50 to nav-links

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-secondary">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
         <div className="container-fluid">
             <a className="navbar-brand text-light fw-bold" href="#">Start Bootstrap</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,9 +11,9 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
                 <a className="nav-link active text-light fw-bold" aria-current="page" href="#">Home</a>
-                <a className="nav-link text-light fw-bold" href="#">About</a>
-                <a className="nav-link text-light fw-bold" href="#">Services</a>
-                <a className="nav-link text-light fw-bold" href="#">Contact</a>
+                <a className="nav-link text-light opacity-50" href="#">About</a>
+                <a className="nav-link text-light opacity-50" href="#">Services</a>
+                <a className="nav-link text-light opacity-50" href="#">Contact</a>
             </div>
             </div>
         </div>
