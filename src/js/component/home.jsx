44513505b6ede81d09cf800d44ef9b6be443e400
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-04 14:08:18 +0000

    Project Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,50 +1,36 @@
 import React from "react";
 import Footer from "./footer.jsx";
-import Navbar from "./navbar.jsx"
+import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Card from "./card.jsx";
 
 
-
-//create your first component
 const Home = () => {
-	const cards = [1, 2, 3, 4]; 
-	const cardComponents = []; 
-    
-	for (let i = 0; i < cards.length; i++) {
-	  cardComponents.push(<Card key={cards[i]} />);
-
-	}
+  const cards = [1, 2, 3, 4];
   
-	
   return (
     <div>
-    <div className="g-0">
-      <Navbar />
-    </div>
-      <div className="container p-5 mt-4 rounded" style={{backgroundColor: "#E0E0E0"}}>
+      <div className="g-0">
+        <Navbar />
+      </div>
+      <div
+        className="container p-5  rounded"
+        style={{ backgroundColor: "#E0E0E0" }}
+      >
         <Jumbotron />
       </div>
-  <div className="container g-0">
+      <div className="container g-0">
         <div className="row">
-
-          <div className="col-lg-3 col-md-6 mb-4 pt-3">
-            <div className="card">{cardComponents[0]}</div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4 pt-3">
-            <div className="card">{cardComponents[1]}</div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4 pt-3">
-            <div className="card">{cardComponents[2]}</div>
-          </div>
-          <div className="col-lg-3 col-md-6 mb-4 pt-3">
-            <div className="card">{cardComponents[3]}</div>
-          </div>
+          {cards.map((card) => (
+            <div className="col-lg-3 col-md-6 mb-4 pt-3" key={card}>
+              <Card />
+            </div>
+          ))}
         </div>
       </div>
       <div>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
     </div>
   );
 };
